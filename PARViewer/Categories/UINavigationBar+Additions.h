89092eb0a52d90bd1eed6aefe0108776f84b93ab
Grayson Sharpe <grayson.g.sharpe@gmail.com>
2013-02-06 21:08:11 -0600

    Added UINavigationBar+Additions to build… my bad...

---
+++ PARViewer/Categories/UINavigationBar+Additions.h
@@ -0,0 +1,14 @@
+//
+//  UINavigationBar+Additions.h
+//  PARViewer
+//
+//  Created by Grayson Sharpe on 2/6/13.
+//  Copyright (c) 2013 Ben Gotow. All rights reserved.
+//
+
+
+@interface UINavigationBar (Additions)
+
+- (void)addShadowEffect;
+
+@end
---
+++ PARViewer/Categories/UINavigationBar+Additions.m
@@ -0,0 +1,19 @@
+//
+//  UINavigationBar+Additions.m
+//  PARViewer
+//
+//  Created by Grayson Sharpe on 2/6/13.
+//  Copyright (c) 2013 Ben Gotow. All rights reserved.
+//
+
+#import "UINavigationBar+Additions.h"
+
+@implementation UINavigationBar (Additions)
+
+- (void)addShadowEffect{
+    [[self layer] setShadowRadius: 3];
+    [[self layer] setShadowOffset: CGSizeMake(0, 1)];
+    [[self layer] setShadowOpacity: 0.3];
+}
+
+@end
