e33572aae76b1011bd3737dfc5fe0bde288a388c
Ben Gotow <bengotow@gmail.com>
2013-02-11 22:56:09 -0600

    Addition / update of 1x assets, turned off bounciness of sidebar view, which was causing some weirdness

--- PARViewer/PVAppDelegate.m
+++ PARViewer/PVAppDelegate.m
@@ -52,6 +52,7 @@ NSString *const FBSessionStateChangedNotification = @"com.parworks.parviewer.Log
     
     _sidebarController = [[PVSidebarViewController alloc] init];
     _slidingViewController = [[JSSlidingViewController alloc] initWithFrontViewController: [_contentControllers objectAtIndex: 0] backViewController: _sidebarController];
+    _slidingViewController.useBouncyAnimations = NO;
     _slidingViewController.delegate = self;
     self.window.rootViewController = _slidingViewController;
     [self.window makeKeyAndVisible];
