d3a37cf005afa528608dd4ee5cab7014dcabb68d
Grayson Sharpe <grayson.g.sharpe@gmail.com>
2013-02-19 11:09:05 -0600

    Added Black styling to PVLoadingView as well as ability to change back to white

--- PARViewer/Views/PVLoadingView.h
+++ PARViewer/Views/PVLoadingView.h
@@ -9,11 +9,18 @@
 #import <UIKit/UIKit.h>
 #import <QuartzCore/QuartzCore.h>
 
+typedef enum PVLoadingViewStyle{
+    PVLoadingViewStyleWhite = 0,
+    PVLoadingViewStyleBlack = 1,
+} PVLoadingViewStyle;
+
 @interface PVLoadingView : UIView
 {
     UIView * _block1;
     UIView * _block2;
 }
+
+@property (nonatomic) PVLoadingViewStyle loadingViewStyle;
 
 - (void)startAnimating;
 - (void)stopAnimating;
