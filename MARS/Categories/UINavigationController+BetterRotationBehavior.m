04a1155c8f32de55e01ea5cb5e36fc11b0df5f5f
Demetri Miller <demetri.miller@gmail.com>
2013-04-24 17:37:20 -0500

    Fixed webviewcontroller rotation bug in core; Updated submodule

--- MARS/Categories/UINavigationController+BetterRotationBehavior.m
+++ MARS/Categories/UINavigationController+BetterRotationBehavior.m
@@ -23,21 +23,21 @@
 
 - (BOOL)shouldAutorotate
 {
-    if ([self.topViewController respondsToSelector:@selector(shouldAutorotate)])
+    if (self.topViewController && [self.topViewController respondsToSelector:@selector(shouldAutorotate)])
         return self.topViewController.shouldAutorotate;
     return YES;
 }
 
 - (NSUInteger)supportedInterfaceOrientations
 {
-    if ([self.topViewController respondsToSelector: @selector(supportedInterfaceOrientations:)])
+    if (self.topViewController && [self.topViewController respondsToSelector: @selector(supportedInterfaceOrientations)])
         return [self.topViewController supportedInterfaceOrientations];
     return UIInterfaceOrientationMaskPortrait;
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    if ([self.topViewController respondsToSelector: @selector(shouldAutorotateToInterfaceOrientation:)])
+    if (self.topViewController && [self.topViewController respondsToSelector: @selector(shouldAutorotateToInterfaceOrientation:)])
         return [self.topViewController shouldAutorotateToInterfaceOrientation: interfaceOrientation];
     return UIInterfaceOrientationIsPortrait(interfaceOrientation);
 }
